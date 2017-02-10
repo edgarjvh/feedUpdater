ec08e341d97dee540ad870bc22fe7b9af7798a8b
Edgar Villasmil <edgarjvh@gmail.com>
2017-02-10 02:58:46 -0400

    Feed Updater

---
+++ Feeds Updater/frmFeedUpdater.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Feeds_Updater
+{
+    partial class frmFeedUpdater
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFeedUpdater));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.NotFeedUpdater = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.hideToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 98);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.Color.LightCoral;
+            this.lblStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(0, 79);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(289, 23);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Feed Updater Stopped";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(29, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(87, 50);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // NotFeedUpdater
+            // 
+            this.NotFeedUpdater.ContextMenuStrip = this.contextMenuStrip1;
+            this.NotFeedUpdater.Icon = ((System.Drawing.Icon)(resources.GetObject("NotFeedUpdater.Icon")));
+            this.NotFeedUpdater.Text = "Feed Updater";
+            this.NotFeedUpdater.Visible = true;
+            this.NotFeedUpdater.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(173, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(87, 50);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // hideToolStripMenuItem
+            // 
+            this.hideToolStripMenuItem.Name = "hideToolStripMenuItem";
+            this.hideToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hideToolStripMenuItem.Text = "Hide";
+            this.hideToolStripMenuItem.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Enabled = false;
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // frmFeedUpdater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 102);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(305, 141);
+            this.MinimumSize = new System.Drawing.Size(305, 141);
+            this.Name = "frmFeedUpdater";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Feed Updater";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmFeedUpdater_FormClosing);
+            this.Resize += new System.EventHandler(this.frmFeedUpdater_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NotifyIcon NotFeedUpdater;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+    }
+}
---
+++ Feeds Updater/frmFeedUpdater.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Feeds_Updater
+{
+    public partial class frmFeedUpdater : Form
+    {
+        wService.soccersoddsSoapClient ws;
+        static string regDir = @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\SportFeeds";
+        string strConn = "";
+
+        public frmFeedUpdater()
+        {
+            InitializeComponent();
+            ws = new wService.soccersoddsSoapClient();
+        }
+
+        private void hideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void frmFeedUpdater_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                strConn = Registry.GetValue(regDir, "strConn", "").ToString();
+
+                if (!strConn.Equals(""))
+                {
+                    using (SqlConnection conn = new SqlConnection(strConn))
+                    {
+                        conn.Open();
+
+                        string query1 = "select * from countries";
+                        SqlCommand cmd1 = new SqlCommand(query1, conn);
+                        SqlDataAdapter da1 = new SqlDataAdapter(cmd1);
+                        DataSet ds1 = new DataSet();
+                        da1.Fill(ds1);
+
+                        if (ds1.Tables[0].Rows.Count > 0)
+                        {
+                            // loop all countries looking for corresponding urls of odds, tvstations and livescores
+                            for (int i = 0; i < ds1.Tables[0].Rows.Count; i++)
+                            {
+                                if (backgroundWorker1.CancellationPending)
+                                {
+                                    e.Cancel = true;
+                                }
+
+                                string query2 = "select * from feedurls where country_id = @1";
+                                SqlCommand cmd2 = new SqlCommand(query2, conn);
+                                cmd2.Parameters.AddWithValue("@1", (int)ds1.Tables[0].Rows[i]["country_id"]);
+                                SqlDataAdapter da2 = new SqlDataAdapter(cmd2);
+                                DataSet ds2 = new DataSet();
+                                da2.Fill(ds2);
+
+                                if (ds2.Tables[0].Rows.Count > 0)
+                                {
+                                    bool existsOdds = false;
+
+                                    // loop the dataset of feed urls looking for a match with type "odds"
+                                    for (int x = 0; x < ds2.Tables[0].Rows.Count; x++)
+                                    {
+                                        if (backgroundWorker1.CancellationPending)
+                                        {
+                                            e.Cancel = true;
+                                        }
+
+                                        // if there are odds, call the web service to retrieve the data
+                                        if (ds2.Tables[0].Rows[x]["feed_type"].ToString().Equals("odds"))
+                                        {
+                                            ws.SaveSoccerOddsLivescore(ds2.Tables[0].Rows[x]["url"].ToString());
+                                            existsOdds = true;
+                                            break;
+                                        }
+                                    }
+
+                                    if (!existsOdds) return;
+
+                                    // loop the dataset of feed urls looking for a match with type "tvStations"
+                                    for (int x = 0; x < ds2.Tables[0].Rows.Count; x++)
+                                    {
+                                        if (backgroundWorker1.CancellationPending)
+                                        {
+                                            e.Cancel = true;
+                                        }
+
+                                        // if there are tvstations, call the web service to retrieve the data
+                                        if (ds2.Tables[0].Rows[x]["feed_type"].ToString().Equals("tvStations"))
+                                        {
+                                            ws.saveSoccerTvStations(ds2.Tables[0].Rows[x]["url"].ToString());
+                                            break;
+                                        }
+                                    }
+
+                                    // loop the dataset of feed urls looking for a match with type "livescore"
+                                    for (int x = 0; x < ds2.Tables[0].Rows.Count; x++)
+                                    {
+                                        if (backgroundWorker1.CancellationPending)
+                                        {
+                                            e.Cancel = true;
+                                        }
+
+                                        // if there are livescores, call the web service to retrieve the data
+                                        if (ds2.Tables[0].Rows[x]["feed_type"].ToString().Equals("livescore"))
+                                        {
+                                            ws.SaveSoccerOddsLivescore(ds2.Tables[0].Rows[x]["url"].ToString());
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btnStart.Enabled = btnStop.Enabled;
+            btnStop.Enabled = !btnStart.Enabled;
+            startToolStripMenuItem.Enabled = btnStart.Enabled;
+            stopToolStripMenuItem.Enabled = btnStop.Enabled;
+            lblStatus.Text = "Feed Updater Started";
+            lblStatus.BackColor = Color.LightGreen;
+            Icon = Properties.Resources.iconStarted;
+            NotFeedUpdater.Icon = Properties.Resources.iconStarted;
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btnStop.Enabled = btnStart.Enabled;
+            btnStart.Enabled = !btnStop.Enabled;
+            startToolStripMenuItem.Enabled = btnStart.Enabled;
+            stopToolStripMenuItem.Enabled = btnStop.Enabled;
+            lblStatus.Text = "Feed Updater Stopped";
+            lblStatus.BackColor = Color.LightCoral;
+            Icon = Properties.Resources.iconStopped;
+            NotFeedUpdater.Icon = Properties.Resources.iconStopped;
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnStart_Click(null, null);
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnStop_Click(null, null);
+        }
+
+        private void frmFeedUpdater_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            const string message = "Are you sure to exit?";
+            const string caption = "Message";
+
+            var result = MessageBox.Show(message, caption,
+                             MessageBoxButtons.YesNo,
+                             MessageBoxIcon.Question);
+
+            e.Cancel = (result == DialogResult.No);
+        }
+    }
+}
